0f1124ef55ab6abff23bc6c372b668200bf8438f
JacksonFrank <jacksonrfrank@gmail.com>
2022-04-14 01:27:20 -0400

    User Authentication
    - Added functionality to register users to the database
    - Added functionality to authenticate users with their username and password

--- database_access/db_test.py
+++ database_access/db_test.py
@@ -6,3 +6,10 @@ if (len(sys.argv) == 3):
     db_connection = LCTDB(sys.argv[1], sys.argv[2])
 else:
     print("Need to include username and password to database in cmd line arguments (Form: db_test.py <username> <password>")
+    sys.exit()
+
+db_connection.registerUser("jackson", "abc123")
+db_connection.authenticateUser("jackson", "abc123")
+db_connection.authenticateUser("jackson", "wrongpassword")
+
+db_connection.closeCon()
--- database_access/lctdb.py
+++ database_access/lctdb.py
@@ -4,15 +4,30 @@
 import psycopg2
 
 class LCTDB: 
-    
+
     def __init__(self, username: str, password: str):
         print("Initializing database connection...")
 
         try:
-            self.conn = psycopg2.connect("dbname=lctdb user=" + username + " password=" + password + " host=localhost")
+            self.con = psycopg2.connect("dbname=lctdb user=" + username + " password=" + password + " host=localhost")
         except:
             sys.exit("Could not connect to the database")
         
         print("Connected to Database!")
+
+    def closeCon(self):
+        self.con.close()
     
-    
+    def registerUser(self, username: str, password: str):
+        cur = self.con.cursor()
+        sql = """CALL register_user(%s, %s);"""
+        cur.execute(sql, (username, password))
+        self.con.commit()
+        cur.close()
+
+    def authenticateUser(self, username: str, password: str):
+        cur = self.con.cursor()
+        cur.callproc('authenticate_user', (username, password))
+        result = cur.fetchall()
+        print(result)
+        cur.close()
