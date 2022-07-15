1219ba27a0315050c23b2cc50e1655b840e9c984
Jackson Frank <jacksonrfrank@gmail.com>
2022-07-14 21:41:55 -0400

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask #从flask包导入Flask类
 
 app = Flask(__name__) #实例化
 
-app.config['SECRET_KEY'] = '06fcd6fc633843ab6f98292a3ccbb5a3' #set secret key; protect against attack, etc.
+app.config['SECRET_KEY'] = <SECRET_KEY> #set secret key; protect against attack, etc.
 
 from flaskblog import routes
 
